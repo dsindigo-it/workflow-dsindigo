1ec3c710537b2c7cc93cf0d268746932dfc8f218
Jorge Ramón <jorger@dsindigo.mx>
2017-05-09 09:58:37 -0500

    Se corrige la formula

--- jorge/dimensions/solution.groovy
+++ jorge/dimensions/solution.groovy
@@ -1,5 +1,5 @@
 // 2*l*w + 2*w*h  + 2*hl * 
-Closure formula = { l, w, h -> (2*l*w) + (2*l*w) + (2*h*l)  }
+Closure formula = { l, w, h -> (2*l*w) + (2*w*h) + (2*h*l)  }
 int result = 0;
 new File("dimesions.txt").text.eachLine {
     def values = it.split("x")*.toInteger()
