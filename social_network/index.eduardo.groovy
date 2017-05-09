aaea2d15ebccc92063335e0a5420f9e40526c1f5
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2017-05-09 10:43:50 -0500

    nuevos archivos propios html y groovy

--- social_network/index.eduardo.groovy
+++ social_network/index.eduardo.groovy
@@ -1,5 +1,5 @@
 def engine   = new groovy.text.SimpleTemplateEngine()
-def source   = getClass().classLoader.getResource('index.html')
+def source   = getClass().classLoader.getResource('index.eduardo.html')
 def template = engine.createTemplate(source)
 def model = [message: "Hello world ${new Date()}"]
 out << template.make(model)
