c5502d6c114dde51fd5529a6aa2b21381d90d93e
Jorge Ramón <jorger@dsindigo.mx>
2017-05-09 10:16:02 -0500

    Se optimiza código

--- jorge/dimensions/solution.groovy
+++ jorge/dimensions/solution.groovy
@@ -1,4 +1,4 @@
 // 2*l*w + 2*w*h  + 2*hl * 
 
-Closure formula = { l, w, h -> (2*l*w) + (2*w*h) + (2*h*l) + Math.min(Math.min(l*w, w*h), h*l)
-println new File("dimesions.txt").collect { it.split("x")*.toInteger() }.collect { formula(*it) }.sum()
+Closure formula = { l, w, h -> (2*l*w) + (2*w*h) + (2*h*l) + Math.min(Math.min(l*w, w*h), h*l) }
+println new File("dimesions.txt").collect { formula(*it.split("x")*.toInteger()) }.sum()
