621d6b9e568c6559437e347af7c63db98b021cbd
Jorge Ramón <jorger@dsindigo.mx>
2017-05-09 09:56:52 -0500

    Primera aproximación a la solución

---
+++ jorge/dimensions/solution.groovy
@@ -0,0 +1,8 @@
+// 2*l*w + 2*w*h  + 2*hl * 
+Closure formula = { l, w, h -> (2*l*w) + (2*l*w) + (2*h*l)  }
+int result = 0;
+new File("dimesions.txt").text.eachLine {
+    def values = it.split("x")*.toInteger()
+    result += formula(*values) + values.min()
+}
+println "Resultado: ${result}"
