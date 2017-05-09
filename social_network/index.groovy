f5db8ac0d7556a8e1a00866df14652f44d3b3cdf
neodevelop <juan@makingdevs.com>
2017-05-09 10:12:17 -0500

    Agregando primer parte de la app

--- social_network/index.groovy
+++ social_network/index.groovy
@@ -1,5 +1,5 @@
 def engine   = new groovy.text.SimpleTemplateEngine()
 def source   = getClass().classLoader.getResource('index.html')
 def template = engine.createTemplate(source)
-def model = [message: "Hello world"]
-out << template.make(result:result)
+def model = [message: "Hello world ${new Date()}"]
+out << template.make(model)
