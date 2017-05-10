b7060231885975d3019849dae17baefa4e162b19
neodevelop <juan@makingdevs.com>
2017-05-10 11:30:05 -0500

    SN-7 Agregando script de prueba

--- social_network/com/makingdevs/service/UserService.groovy
+++ social_network/com/makingdevs/service/UserService.groovy
@@ -7,8 +7,8 @@ class UserService {
     def sql = groovy.sql.Sql.newInstance("jdbc:h2:social_network","sa","sa","org.h2.Driver")
 
     def getUsers() {
-        sql.eachRow("SELECT * FROM user") {
-            println new Usuario(it)
-        }
+     //   sql.eachRow("SELECT * FROM user") {
+     //       println new Usuario(it)
+     //   }
     }
 }
