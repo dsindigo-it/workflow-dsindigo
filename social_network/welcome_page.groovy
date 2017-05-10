39cff7d5f6e0b8ada376b7001271279932f5216d
neodevelop_indigo <neodevelop@dsindigo.com>
2017-05-10 12:46:03 -0500

    SN-17 corrigiendo mensaje de bienvenida

--- social_network/welcome_page.groovy
+++ social_network/welcome_page.groovy
@@ -2,11 +2,18 @@ import com.makingdevs.model.Usuario
 def engine   = new groovy.text.SimpleTemplateEngine()
 def source   = getClass().classLoader.getResource('welcome_page.html')
 def template = engine.createTemplate(source)
+
+
 Usuario usuario = new Usuario()
   usuario.userName = "usernametest"
   usuario.fullName = "Usuario full name"
   usuario.email = "usuario@servidorusuarios.com"
   usuario.dateCreated = new Date()
 
-def model = [userfullname: "${usuario.fullName}"]
+String message = "Porfavor inicia sesión"
+
+if(session?.user)
+  message = "Bienvenido"
+
+def model = [message:message, user:usuario]
 out << template.make(model)
