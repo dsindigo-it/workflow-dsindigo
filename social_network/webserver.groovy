dcc6d7a35c6167ab69badc26d01c32db93ac9fb1
neodevelop <juan@makingdevs.com>
2017-05-10 10:44:42 -0500

    SN-8 Agregando manejador de bases:w

--- social_network/webserver.groovy
+++ social_network/webserver.groovy
@@ -1,6 +1,9 @@
+@GrabConfig(systemClassLoader=true)
 @Grab('org.eclipse.jetty.aggregate:jetty-server:8.1.9.v20130131')
 @Grab('org.eclipse.jetty.aggregate:jetty-servlet:8.1.9.v20130131')
 @Grab('javax.servlet:javax.servlet-api:3.0.1')
+@Grab(group='com.h2database', module='h2', version='1.3.176')
+
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.*
 import groovy.servlet.*
@@ -11,3 +14,9 @@ def context = new ServletContextHandler(server, "/", SESSIONS)
 context.resourceBase = "."
 context.addServlet(GroovyServlet, "*.groovy")
 server.start()
+
+def sql = groovy.sql.Sql.newInstance("jdbc:h2:social_network","sa","sa","org.h2.Driver")
+
+sql.execute("""
+create table if not exists data(id integer primary key, campo varchar(255))
+""")
